14a891d41103f3a4381731442b982a8069537027
carysklt <56458748+carysklt@users.noreply.github.com>
2020-12-10 20:55:00 +0800

    Create login.jsx

---
+++ frontend/src/login.jsx
@@ -0,0 +1,94 @@
+import GoogleLogin from 'react-google-login'
+
+class Login extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            locations: [],
+            user:null
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        const userRef = firebase.database().ref('users');
+        const user = {
+            currentUser: this.state.username,
+            currentPassword: this.state.password
+        }
+        userRef.push(user);
+        this.setState({
+            username: '',
+            password: ''
+        });
+    }
+
+    // login(){
+    //     //provider should be Google Auth Provider
+    //     auth.signInWithPopup(provider)
+    //         .then((result)=>{
+    //             const user = result.user;
+    //             this.setState({
+    //                 user
+    //             });
+    //         });
+    // }
+
+    // logout(){
+    //     auth.signOut()
+    //         .then(()=>{
+    //             this.setState({
+    //                 user:null
+    //             });
+    //         });
+    // }
+
+    responseGoogle=(response)=>{
+        console.log(response);
+        console.log(response.profileObj);
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <h2>Sign In</h2>
+                <p>If you don't have an account, click 'Register' below or sign-in using Google!</p>
+                <input 
+                    type="text" 
+                    ref="username" 
+                    placeholder="username"
+                    onChange={this.handleChange}
+                    value={this.state.username}
+                />
+                <input 
+                    type="password" 
+                    ref="password" 
+                    placeholder="password"
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                />
+                <input type="submit" value="login"/>
+                <GoogleLogin
+                    //!!need ClientID from Firebase
+                    clientId = ''
+                    buttonText = "Login"
+                    onSuccess = {this.responseGoogle}
+                    onFailure = {this.responseGoogle}
+                    cookiePolicy = {'single_host_origin'}
+                />
+            </form>
+        )
+    }
+} 
+
+ReactDOM.render(<App/>, document.getElementById('app'));
