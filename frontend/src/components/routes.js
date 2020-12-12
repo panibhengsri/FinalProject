a00a948c36788d5637347e3b51e4f8dde6d76cee
carysklt <56458748+carysklt@users.noreply.github.com>
2020-12-12 20:26:15 +0800

    Update routes.js

--- frontend/src/components/routes.js
+++ frontend/src/components/routes.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Route, IndexRoute } from 'react-router-dom';
+// import React from 'react';
+// import { Route, IndexRoute } from 'react-router-dom';
 
-// import App from './App.js';
-import Login from './components/login.js';
-import Locations from './components/locations.js';
-import Result from './components/result.js';
+// // import App from './App.js';
+// import Login from './components/login.js';
+// import Locations from './components/locations.js';
+// import Result from './components/result.js';
 
-export default(
-    <Route path="/" component={Login}>
-        <Route path="/locations" component={Locations}/>
-        <Route path="/result" component={Result}/>
-    </Route>
-);
+// export default(
+//     <Route path="/" component={App}>
+//         <Route path="/locations" component={Locations}/>
+//         <Route path="/result" component={Result}/>
+//     </Route>
+// );
