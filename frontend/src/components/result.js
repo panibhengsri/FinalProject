8ba0c6348e6927d046dd6ec299c68f4c40ada5e5
carysklt <56458748+carysklt@users.noreply.github.com>
2020-12-12 16:23:14 +0800

    Update result.js

--- frontend/src/components/result.js
+++ frontend/src/components/result.js
@@ -1,1 +1,34 @@
 import React from 'react';
+import { render } from 'react-dom';
+
+export class Result extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            details: false
+        }
+    }
+
+    learnMore() {
+        this.setState({details: this.state.details});
+    }
+
+    render(){
+        return(
+            <div>
+                 <div class="simpleResult">
+                    <h2>8/10</h2>
+                    <p>You should leave the house today!</p>
+                     {/* the score out of ten */}
+                     {/* statement that says whether user should leave the house */}
+            
+                 </div>
+                <button onClick={this.learnMore}>see why</button>
+            </div>
+        )
+    }
+
+
+}
+
+export default Result;
