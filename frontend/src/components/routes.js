23720a62edf6f0ca299aee27600f32bf4d542c5c
carysklt <56458748+carysklt@users.noreply.github.com>
2020-12-14 23:12:57 +0800

    Update routes.js

--- frontend/src/components/routes.js
+++ frontend/src/components/routes.js
@@ -23,3 +23,31 @@ const Routes = () => {
 
 // )
 }
+
+// pani's example
+// import React from 'react'
+// import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// import CoordRoute from './modules/logIn/CoordRoute.js'
+// import AdminRoute from './modules/logIn/AdminRoute.js'
+// import notFound from './modules/logIn/NotFound.js'
+// import Login from './modules/logIn/index.js'
+
+// import adminAddSite from './modules/admin/adminAddSite'
+// import adminHomepage from './modules/admin/adminHomepage'
+// import adminViewSite from './modules/admin/adminViewSite'
+// import adminManageSite from './modules/admin/adminManageSite'
+
+// import siteOverview from './modules/coords/siteOverview'
+// import detailView from './modules/coords/detailView'
+// import siteAddCase from './modules/coords/siteAddCase'
+
+// const Routes = () => {
+// return (
+// <div className="auth0button">
+// {(
+// <span>
+// <BrowserRouter>
+// <Switch>
+// <Route exact path="/" component = {Login} type = "login"/>
+// <AdminRoute exact path="/admin" component = {adminHomepage} type = "adminov
