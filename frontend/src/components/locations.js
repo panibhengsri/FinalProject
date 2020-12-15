e50f2f3191893df54d21d71c17192d45c9802ebc
carysklt <56458748+carysklt@users.noreply.github.com>
2020-12-15 23:28:45 +0800

    Update locations.js

--- frontend/src/components/locations.js
+++ frontend/src/components/locations.js
@@ -1,24 +1,40 @@
+/*  Login contains <SignIn> "button"
+    Edited on 10/12/2020 by Duncan Chang, Jeremy Jung
+*/
 import React from 'react';
-import ReactDOM from "react-dom";
-import {
-    BrowserRouter as Router, Route
-} from "react-router-dom";
-import Login from './login.js';
 
-// ReactDOM.render(
-    
-// );
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
 
-class Locations extends React.Component {
-    constructor (props) {
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+// import GoogleLogin from 'react-google-login';    // COMMENTED BY JEREMY JUNG
+import SignIn from './SignIn.js';
+import SignOut from './SignOut.js';
+import AddList from './AddList.js';
+
+import Logo from './logo.png';
+
+class Locations extends React.Component{
+    constructor(props) {
         super(props);
     }
 
-    render () {
-        return (
-            <div>
-                Testing locations
+    render(){
+        return(
+		<div className="container">
+            
+                <section>
+                {this.props.user ? <AddList firestore={this.props.firestore} auth = {this.props.auth} /> : <SignIn firestore = {this.props.firestore} auth = {this.props.auth}/>}
+            </section>
+            <div> 
+                <SignOut auth = {this.props.auth} />
             </div>
+        </div>
+        
         )
     }
 }
